210959c3532bcc6baf28240ad7d8a11b76d5e299
deeZNUTS831 <sumes8668@gmail.com>
2025-05-20 11:22:02 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+red.addEventListener('click', () => {
+    document.body.style.backgroundColor = "red";
+})
+
+green.addEventListener('click', () => {
+    document.body.style.backgroundColor = "green";
+})
+
+blue.addEventListener('click', () => {
+    document.body.style.backgroundColor = "blue";
+})
+
+yellow.addEventListener('click', () => {
+    document.body.style.backgroundColor = "yellow";
+})
+
+
+white.addEventListener('click', () => {
+    document.body.style.backgroundColor = "white";
+})
